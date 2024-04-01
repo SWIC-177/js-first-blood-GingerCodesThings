c5e6454c9830b608695040b5bb3715e92aa2b182
Kailee M <maurer.kailee@gmail.com>
2024-04-01 18:58:27 -0500

    second part of the assignment

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
-const person = {
-    name: "John",
-    age: 17,
-  };
-   
-    /* changed personName and personAge to person. personName and personAge 
-  is the incorrect way to call the name and age attributes 
-  */ 
+const people = [
+    {
+      name: "John",
+      age: 17,
+    },
+    {
+      name: "Jane",
+      age: 20,
+    },
+    {
+      name: "Mary",
+      age: 25,
+    },
+    {
+      name: "Peter",
+      age: 30,
+    },
+    {
+      name: "Paul",
+      age: 35,
+    },
+  ]; 
+
+let i = 0;
   
-  function verifyAdulthood(person) {
-    if (person.age >= 18) return `Welcome, ${person.name}!`;
+  function verifyAdulthood(people, i) {
+    
+     if (people[i].age >= 18) return `Welcome, ${people[i].name}!`;
   
-    return `You are not old enough to enter, ${person.name}.`;
-  }
+    return `You are not old enough to enter, ${people[i].name}.`;
+}
+
+while (i < people.length){
+    console.log(verifyAdulthood(people,i));
+    i++;
+}
   
-  const adultMessage = verifyAdulthood(person);
+
+
   
-  console.log(adultMessage);
   
